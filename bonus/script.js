467387da26b7e081419842a75f9574bffb9d86b6
DamianoMura <mura.damiano14@gmail.com>
2025-07-03 17:08:01 +0200

    [add] BONUS - point 3

--- bonus/script.js
+++ bonus/script.js
@@ -10,13 +10,21 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = teachers.reverse(); 
 console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames=[];
 
+for(let x=0 ; x<teachers.length ; x++){
+  if(teachers[x].length>=5)
+  {
+ longNames.push(teachers[x]);
+  }
+}
+
+console.log(longNames);
 // 3. Rimuovi 'Ed' dall'array teachers
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
