b0a5a731cbd7c896e59cc9ad7b1ba96863198aa8
DamianoMura <mura.damiano14@gmail.com>
2025-07-03 17:09:02 +0200

    [add] BONUS - point 5

--- bonus/script.js
+++ bonus/script.js
@@ -33,4 +33,5 @@ const isFabioPresent = teachers.indexOf("Fabio")>=0;
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString);
